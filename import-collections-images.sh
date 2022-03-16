475c8fe5d9855824415c6164e85d014662eed0f0
BFallert <bernd.fallert@bib.uni-mannheim.de>
2022-03-16 09:11:36 +0100

    script simplified

--- import-collections-images.sh
+++ import-collections-images.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
-cd public/fileadmin/
-if [[ -d groups ]]
-then
-    cd groups
-else
-    mkdir groups
-    cd groups
-fi
 
-if [[ -d collections ]]
-then
-    cd collections
-else
-    mkdir collections
-    cd collections
-fi
+mkdir -p public/fileadmin/groups/collections/images/
 
-if [[ -d images ]]
-then
-    cd images
-else
-    mkdir images
-    cd images
-fi
-
-wget -N https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/dlf_hp_VirtuellesKartenforum.jpg https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/newspaper.jpg  https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/ubma.jpg https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/europaeische_rechtsgeschichte.jpg
+wget -N --directory-prefix=public/fileadmin/groups/collections/images/  https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/dlf_hp_VirtuellesKartenforum.jpg https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/newspaper.jpg  https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/ubma.jpg https://digi.bib.uni-mannheim.de/fileadmin/ddev-kitodo-temp/europaeische_rechtsgeschichte.jpg
